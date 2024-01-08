dcc8aa2a1b280e8a8545f3ce72d2a50c72cf5f73
codeSweet0828 <135516294+codeSweet0828@users.noreply.github.com>
2024-01-08 11:33:38 +0900

    [Silver I] Title: 숨바꼭질, Time: 256 ms, Memory: 26764 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,40 +5,38 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+	static boolean[] visited;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		visited = new boolean[1000001];
+		
+		int N =sc.nextInt();
+		int K = sc.nextInt();
+		int[] arr = new int[100001];  
+		Queue<Integer> q = new LinkedList();
+		q.add(N);
+		arr[N] = 0;
 
-        //수빈이 위치
-        int N = sc.nextInt();
-        
-        //동생의 위치
-        int K = sc.nextInt();
+		while(!q.isEmpty()) {
+			int current = q.poll();
+			
+			if(current == K) {
+				System.out.println(arr[K]);
+				break;
+			}
+			
+			int [] nextPositions = {current-1, current+1, current*2 };
+			for(int next : nextPositions) {
+				if (next >= 0 && next <= 100000 && arr[next] == 0) {
+					q.add(next);
+					arr[next] = arr[current]+1;
+				}
+			}
+		}
+		
 
-        int[] arr = new int[100001];  
-        Queue<Integer> queue = new LinkedList<>();
+	}
 
-        queue.add(N);
-        arr[N] = 0;
-
-        while (!queue.isEmpty()) {
-            int q = queue.poll();
-
-            //반환된 위치가 같다면
-            if (q == K) {
-                System.out.println(arr[q]);
-                break;
-            }
-
-            //다음 포지션 가능위치, Q-1, Q+1, 2*Q
-            int[] nextPositions = {q - 1, q + 1, 2 * q};
-
-            for (int next : nextPositions) {
-                if (next >= 0 && next <= 100000 && arr[next] == 0) {
-                    queue.add(next);
-                    arr[next] = arr[q] + 1;
-                }
-            }
-        }
-    }
 }
