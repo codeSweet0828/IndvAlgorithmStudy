9703040203795e000fc82f1c6addc05a16014d93
Eunsol Kim <135516294+codeSweet0828@users.noreply.github.com>
2024-02-07 09:34:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,24 +1,17 @@
--- 코드를 입력하세요
-SELECT 
-    NAME
-    , DATETIME
-FROM(
-    SELECT 
+SELECT * FROM
+(
+    SELECT
         NAME
-       , DATETIME
-      , ROWNUM AS RN
-    FROM(
+        , DATETIME
+    FROM
+        ANIMAL_INS
+    WHERE
+        ANIMAL_ID 
+    NOT IN (
         SELECT
-            NAME
-            , DATETIME
-        FROM 
-            ANIMAL_INS
-        WHERE ANIMAL_ID NOT IN(
-            SELECT
-                ANIMAL_ID
-            FROM ANIMAL_OUTS 
-        )
-        ORDER BY DATETIME
-    )T1
-)T2
-WHERE T2.RN BETWEEN 1 AND 3
+            ANIMAL_ID
+        FROM
+            ANIMAL_OUTS 
+    ) ORDER BY DATETIME ASC
+)
+WHERE ROWNUM BETWEEN 1 AND 3
