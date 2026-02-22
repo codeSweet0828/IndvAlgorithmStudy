7d8d7e9bba8b4868c7c991e70ee745feb2c01bb3
Eunsol Kim <135516294+codeSweet0828@users.noreply.github.com>
2026-02-23 01:25:34 +0900

    [level 2] Title: 기능개발, Time: 0.38 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,39 +1,29 @@
 import java.util.*;
 class Solution {
-     int[] newArr;
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        List<Integer> list = new ArrayList<>();
-        int prevDay = (int)Math.ceil((100.0- progresses[0])/speeds[0]);
-        int count = 1;
-              
-        for(int i = 1 ; i < progresses.length ;i ++){
-            int nowDay = (int)Math.ceil((100.0- progresses[i])/speeds[i]);
-            
-            if(nowDay <= prevDay){
-                count++;
-            }
-            else{
-                
-                //배포갯수 더하기
-                list.add(count);
-                count = 1;
-                
-                //기준 초기화
-                prevDay = nowDay;
-            }
-            
-        }
-        
-        //마지막 배포갯수 더하기
-        list.add(count);
   
-        answer = new int[list.size()];
+    public int[] solution(int[] progresses, int[] speeds) {
+       
+        List<Integer> list = new ArrayList<>();
+        int prevDay = (int)Math.ceil((100.0-progresses[0])/speeds[0]);
+        int count =1;
         
-        for(int i= 0; i< list.size() ;i++){
-            answer[i] = list.get(i);
+        for(int i = 1 ; i < progresses.length; i++){
+            int nowdays = (int)Math.ceil((100.0-progresses[i])/speeds[i]);
+            
+            if(prevDay >= nowdays){
+                count++;
+            }else{
+                list.add(count);
+                count =1;
+                prevDay = nowdays;
+            }
         }
+        list.add(count);
         
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size() ; i++){
+           answer[i] = list.get(i);
+        }
   
         return answer;
         
